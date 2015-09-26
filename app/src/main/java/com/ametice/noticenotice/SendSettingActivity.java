3c8d9d5b0bfb4cd5d6107d49b5301d82f530fb1c
Kemer-san <mr.ball.touch@gmail.com>
2015-09-26 13:23:59 +0900

    20150926_kemer-san：画面遷移の実装中

---
+++ app/src/main/java/com/ametice/noticenotice/SendSettingActivity.java
@@ -0,0 +1,20 @@
+package com.ametice.noticenotice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Masato on 2015/09/26.
+ * 送信設定画面の動作
+ */
+public class SendSettingActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.send_setting);
+    }
+ }
+
