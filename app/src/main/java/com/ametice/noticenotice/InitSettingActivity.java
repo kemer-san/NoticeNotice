5b741e872ced8d82a1ee362b0b6d2fb23107e5c9
kenzosuzuki <kenzozozozozozozozozozozo@gmail.com>
2015-10-24 16:30:35 +0900

    画面遷移追加・イベントリスナ実装変更 など

--- app/src/main/java/com/ametice/noticenotice/InitSettingActivity.java
+++ app/src/main/java/com/ametice/noticenotice/InitSettingActivity.java
@@ -15,6 +15,23 @@ import android.widget.Toast;
 
 public class InitSettingActivity extends Activity {
 
+    //region "GUI部品"
+
+    private Button btnSendSetting;
+    private Button btnInputAddress;
+    private CompoundButton swOnOff;
+
+    /**
+     * 部品のハンドラを取得します。
+     */
+    private void findComponents() {
+        btnSendSetting = (Button) findViewById(R.id.btnSendSetting);
+        btnInputAddress = (Button) findViewById(R.id.btnInputAddress);
+        swOnOff = (CompoundButton) findViewById(R.id.swOnOff);
+    }
+
+    //endregion
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,54 +39,55 @@ public class InitSettingActivity extends Activity {
         //レイアウトファイルを指定
         setContentView(R.layout.init_setting);
 
+        // 部品のハンドラを取得
+        findComponents();
+
         //送信間隔ボタン押下時のアクション
-        final Button btnSendSetting = (Button) findViewById(R.id.btnSendSetting);
-        btnSendSetting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                //送信間隔設定画面の起動へ
-                Intent intent = new Intent(getApplicationContext(),SendSettingActivity.class);
-<<<<<<< HEAD
-
-=======
->>>>>>> refs/remotes/kemer-san/master
-                startActivity(intent);
-            }
-        });
+        btnSendSetting.setOnClickListener(btnSendSettingOnClickListener);
 
         //送信先設定ボタン押下時のアクション
-        final Button btnInputAddress = (Button) findViewById(R.id.btnInputAddress);
-        btnInputAddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnInputAddress.setOnClickListener(btnInputAddressOnClickListener);
 
-            //送信間隔設定画面の起動へ
-                Intent intent = new Intent(getApplicationContext(),InputAddressActivity.class);
-                startActivity(intent);
+        // OnOffスイッチのアクションをセット
+        swOnOff.setOnCheckedChangeListener(swOnOffOnCheckedChangeListener);
+    }
 
-            }
-        });
+
+    private final Button.OnClickListener btnSendSettingOnClickListener = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            // 送信間隔設定画面の起動
+            Intent intent = new Intent(getApplicationContext(), SendSettingActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private final Button.OnClickListener btnInputAddressOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // 送信先設定画面の起動
+            Intent intent = new Intent(getApplicationContext(),InputAddressActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private final CompoundButton.OnCheckedChangeListener swOnOffOnCheckedChangeListener = new CompoundButton.OnCheckedChangeListener() {
 
         //On-Offスイッチのアクション
         /* <memo by kemer-san>
         ソースコード上ではSwitchもToggleButtonも一括してCompoundButtonとして扱います。
         CompoundButtonはSwitchのように2つの状態を持つViewの抽象クラスで、状態の取得やリスナ関連の処理などが集約されており、
         Switch、ToggleButton、CheckBoxなどはいずれもCompoundButtonを継承したサブクラスになっています。*/
-        CompoundButton swOnOff = (CompoundButton)findViewById(R.id.swOnOff);
-        swOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // 状態が変更されたときのアクション
-                if(isChecked) {
-                    Toast.makeText(InitSettingActivity.this, "通知機能がONになりました", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(InitSettingActivity.this, "通知機能がOFFになりました", Toast.LENGTH_SHORT).show();
-                }
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            // 状態が変更されたときのアクション
+            if (isChecked) {
+                Toast.makeText(InitSettingActivity.this, "通知機能がONになりました", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(InitSettingActivity.this, "通知機能がOFFになりました", Toast.LENGTH_SHORT).show();
             }
-        });
-
-    }
+        }
+    };
 
 
     @Override
