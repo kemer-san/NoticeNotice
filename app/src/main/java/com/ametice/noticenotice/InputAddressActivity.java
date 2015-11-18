a40650ed9c3c2340c0e3d05cf67be4e793668c3a
kemer-san <mr.ball.touch@gmail.com>
2015-11-18 23:25:00 +0900

    Update InputAddressActivity.java

--- app/src/main/java/com/ametice/noticenotice/InputAddressActivity.java
+++ app/src/main/java/com/ametice/noticenotice/InputAddressActivity.java
@@ -5,8 +5,8 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+//import android.widget.Button;
+//import android.widget.EditText;
 
 /**
  * Created by Masato on 2015/09/26.
