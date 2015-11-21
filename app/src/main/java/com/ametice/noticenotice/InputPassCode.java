00e1431df9782f1006046dd95ed83c664aa101c5
Kemer-san <mr.ball.touch@gmail.com>
2015-11-21 15:36:19 +0900

    20151121_kemer-san：パスコードのレイアウト追加

---
+++ app/src/main/java/com/ametice/noticenotice/InputPassCode.java
@@ -0,0 +1,17 @@
+package com.ametice.noticenotice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Masato on 2015/11/21.
+ */
+public class InputPassCode extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //レイアウトファイルを指定
+        setContentView(R.layout.input_passcode);
+    }
+}
